d0e3996b1f1b60df507fb5f0807c1095a99c64ac
Stephanie Baum <sbaum1994@gmail.com>
2016-11-11 09:56:48 -0800

    initial commit

---
+++ src/js/helloworld.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import HelloForm from './HelloForm';
+
+ReactDOM.render(<HelloForm />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+    }
+
+    return sources;
+}
+// production mode will not run the dev webserver ^
+// still will need to update cdn to point to public for production
+// in advanced work you use a templating engine and store as a variable
+// the path to the cdn based on the node env
+// loaders can be regex, they just say hey any js load through jsx loader
+// babel react doesn't need a jsx loader to be separate anymore fyi
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                  presets: ['es2015', 'react']
+                }
+            },
+            // {
+            //     test: /\.scss$/,
+            //     loaders: ['style', 'css', 'sass']
+            // }
+        ]
+    },
+    entry: {
+        helloWorld: getEntrySources([
+            './src/js/helloworld.js'
+        ])
+    },
+    output: {
+        filename: 'public/[name].js'
+    }
+};
